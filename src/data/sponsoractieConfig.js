2256ade46e1a520d0dff8d4f0706afb826272574
Martijn Kerssing <martijn@kerssing.nl>
2026-02-27 21:25:56 +0100

    harde links

--- src/data/sponsoractieConfig.js
+++ src/data/sponsoractieConfig.js
@@ -9,21 +9,14 @@
 // geldig tot: 1 aapril 2026
 
 
-const {
-  NEXT_PUBLIC_SPONSOR_PAYMENT_URL_1,
-  NEXT_PUBLIC_SPONSOR_PAYMENT_URL_2,
-  NEXT_PUBLIC_SPONSOR_PAYMENT_URL_3,
-  NEXT_PUBLIC_SPONSOR_PAYMENT_URL_4,
-  NEXT_PUBLIC_SPONSOR_PAYMENT_URL_5,
-} = process.env;
-
 export const sponsoractieConfig = {
   paymentUrls: [
-    NEXT_PUBLIC_SPONSOR_PAYMENT_URL_1,
-    NEXT_PUBLIC_SPONSOR_PAYMENT_URL_2,
-    NEXT_PUBLIC_SPONSOR_PAYMENT_URL_3,
-    NEXT_PUBLIC_SPONSOR_PAYMENT_URL_4,
-    NEXT_PUBLIC_SPONSOR_PAYMENT_URL_5,
-  ].filter(Boolean),
+"https://www.ing.nl/payreq/m/?trxid=6p1z8a4WRw5hOYAgreBhyBKD2i8YD4He",
+"https://www.ing.nl/payreq/m/?trxid=KKHqMSpXDEdqYpTzsGKJPRfq5tjxdydO",
+"https://www.ing.nl/payreq/m/?trxid=DnLNcDwRBkvQgNE1j0Ilf11RRrCAgDjo",
+"https://www.ing.nl/payreq/m/?trxid=p083zppsFkbzKZa9s0rWHQ9fJTfjg85Y",
+"https://www.ing.nl/payreq/m/?trxid=ooM4UoaAp80AoWz1xwQj4MgYlrx4dcS1",
+
+  ],
   buttonLabel: "Doneer voor de sponsoractie",
 };
