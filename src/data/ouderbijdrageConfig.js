9543f3f73d89a3e3cb540c7825471c2a7826706d
Martijn Kerssing <martijn@kerssing.nl>
2026-05-26 10:14:05 +0200

    Change expiration date and update payment URL
    Updated the expiration date for the payment links and modified one of the URLs.

--- src/data/ouderbijdrageConfig.js
+++ src/data/ouderbijdrageConfig.js
@@ -6,12 +6,12 @@
 // 4: https://www.ing.nl/payreq/m/?trxid=p083zppsFkbzKZa9s0rWHQ9fJTfjg85Y
 // 5: https://www.ing.nl/payreq/m/?trxid=ooM4UoaAp80AoWz1xwQj4MgYlrx4dcS1
 //
-// geldig tot: 1 april 2026
+// geldig tot: 26 juni 2026
 
 
 export const ouderbijdrageConfig = {
   paymentUrls: [
-"https://www.ing.nl/payreq/m/?trxid=LwclbpigDxvL37s6aYNNabpiKHgyLp2N",
+"https://www.ing.nl/payreq/m/?trxid=qcTlp99iFNO4g3YycaH0ICky6SE49hWJ",
 
   ],
   buttonLabel: "Betaal de ouderbijdrage",
